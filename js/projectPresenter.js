547f0a8c3662f9197064343fe57d12317e05f26e
Stefan Prechtl <stfanmprechtl@web.de>
2024-08-03 18:55:14 +0200

    Baume pro Projekt anzeigen

--- js/projectPresenter.js
+++ js/projectPresenter.js
@@ -21,6 +21,16 @@ export default class ProjectPresenter {
         this.view.showProjects(jsonData);
     }
 
+    async loadTree(idProject) {
+        console.log("Lade Bäume vom Server....");
+
+        // Daten per REST-API laden
+        let jsonData = await this.loadTreeFromServer(idProject);
+
+        // View aktualisieren
+        this.view.showTrees(idProject, jsonData);
+    }
+
     /**
      * Projekte per REST-API als Json-String laden
      * @returns json-String mit Liste aller Projekte
@@ -31,6 +41,20 @@ export default class ProjectPresenter {
             let response = await fetch("testdata/projectdata.json");
             let result = await response.json();
             return result;
+        } catch (err) {
+            console.log(`Fehler:${err}`);
+        }
+    }
+
+    async loadTreeFromServer(idProject) {
+        console.log(idProject);
+        try {
+            //let response = await fetch(`http://localhost:8080/monolith/rext/projectmgmt/projects`);
+            let response = await fetch("testdata/treedata.json");
+            let result = await response.json();
+            let filteredResult = result.filter(tree => tree.projectid == idProject);
+
+            return filteredResult;
         } catch (err) {
             console.log(`Fehler:${err}`);
         }
--- js/projectView.js
+++ js/projectView.js
@@ -1,55 +1,99 @@
-import { $, $$ } from './util.js';
-import { html, render } from './node_modules/lit-html/lit-html.js'
+import { $, $$ } from "./util.js";
+import { html, render } from "./node_modules/lit-html/lit-html.js";
 
 export default class ProjectView {
+  /**
+   * Konstruktor
+   * @param {string} idRootElement Selektor für DOM-Element, das für den View als Root-Element verwendet wird
+   */
+  constructor(idRootElement) {
+    this.rootElement = $(idRootElement);
+    this.initEventhandler();
+  }
 
-    /**
-       * Konstruktor
-       * @param {string} idRootElement Selektor für DOM-Element, das für den View als Root-Element verwendet wird
-       */
-    constructor(idRootElement) {
-        this.rootElement = $(idRootElement);
-        this.initEventhandler();
-    }
+  /**
+   * Zugehörigen Presenter mit diesem View "verknüpfen"
+   * @param {ProjectPresenter} presenter
+   */
+  setPresenter(presenter) {
+    this.presenter = presenter;
+  }
 
-    /**
-     * Zugehörigen Presenter mit diesem View "verknüpfen"
-     * @param {ProjectPresenter} presenter
-     */
-    setPresenter(presenter) {
-        this.presenter = presenter;
-    }
+  /**
+   * DOM-Elememt im "View-Teilbaum" (ab View-Root) selektieren
+   * @param {string} selector Selektor für DOM-Selektion
+   * @returns {HTMLElement} Selektiertes HTML-Element
+   */
+  $(selector) {
+    return this.rootElement.querySelector(selector);
+  }
 
-    /**
-       * DOM-Elememt im "View-Teilbaum" (ab View-Root) selektieren
-       * @param {string} selector Selektor für DOM-Selektion
-       * @returns {HTMLElement} Selektiertes HTML-Element
-       */
-    $(selector) {
-        return this.rootElement.querySelector(selector);
-    }
+  $$(selector) {
+    return this.rootElement.querySelectorAll(selector);
+  }
 
-    templateListProject = (projects) => html`
-         <ul class="w3-ul w3-border w3-hoverable">
-         <li><h3>Projekte</h3></li>
-            ${projects.map(project => html`<li datat-id=${project.id}>${project.name}</li>`)}
-        </ul>`;
+  templateListProject = (projects) => html`
+        <div class="w3-container w3-border">
+        <h3>Projekte</h3>
+         <ul id="listProject" class="w3-ul w3-border w3-margin-bottom " @click=${
+           this.handleSelectedProject
+         }>
+         </li>${projects.map(
+           (project) => html`<li data-id=${project.id}>${project.name}<div class="divtreelist" data-id=${project.id}></div></li>`
+         )}
+        </ul>
+        </div>`;
 
-    /**
-     * Alle Events für diesen Views definieren
-     */
-    initEventhandler() {
+  templateListTree = (trees) => html`
+    <ul class="w3-ul w3-border w3-margin-bottom">
+    <li class="w3-grey">Bäume</li>
+    </li>${trees.map((tree) => html`<li data-id=${tree.id}>${tree.name}</li>`)}
+    </ul>`;
 
-        const btnLoad = this.$("#btnLoad");
-        btnLoad.on("click", () => { this.presenter.loadProjects(); });
-    }
+  /**
+   * Alle Events für diesen Views definieren
+   */
+  initEventhandler() {
+    const btnLoad = this.$("#btnLoad");
+    btnLoad.on("click", () => {
+      this.presenter.loadProjects();
+    });
+  }
 
-    /**
-     * Methode wird vom Presenter aktiviert
-     * @param {*} projects 
-     */
-    showProjects(projects) {
-        let divListe = $("#placeholderProjects");
-        render(this.templateListProject(projects), divListe);
-    }
+  /**
+   * Methode wird vom Presenter aktiviert
+   * @param {*} projects Liste der Projekte
+   */
+  showProjects(projects) {
+    let divListe = $("#placeholderProjects");
+    render(this.templateListProject(projects), divListe, {eventContext: this});
+  }
+
+  /**
+   * Methode wird vom Presenter aktiviert
+   * @param {number*} idProject Projekt-ID
+   * @param {*} tree Liste der Bäume des Projekts
+   */
+  showTrees(idProject, trees) {
+
+    let allDivTreelist = $$("div.divtreelist");
+    allDivTreelist.map((div) => {
+        if (div.className == "divtreelist w3-show") {
+            console
+            div.className = "divtreelist w3-hide";
+          };
+        }); 
+
+    let liProjekt = $("div[data-id='"+idProject+"']")
+    liProjekt.className = "divtreelist w3-show";
+    render(this.templateListTree(trees), liProjekt);
+
+  }
+
+  // Event-Handler
+  handleSelectedProject(e) {
+    const idProject = e.target.getAttribute("data-id");
+    console.log(idProject);
+    this.presenter.loadTree(idProject);
+  }
 }
